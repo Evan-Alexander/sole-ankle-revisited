e8542d1f9c279b4dbdf07c31687aeee1b912822f
evan-alexander <jasontbrown99@gmail.com>
2022-05-28 10:01:12 -0700

    adds color theming with vars

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from "react";
 import styled from "styled-components/macro";
 import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { COLORS, QUERIES, WEIGHTS } from "../../constants";
+import { WEIGHTS } from "../../constants";
 
 import UnstyledButton from "../UnstyledButton";
 import Icon from "../Icon";
@@ -42,7 +42,7 @@ const Overlay = styled(DialogOverlay)`
   right: 0;
   left: 0;
   bottom: 0;
-  background: hsl(220deg 5% 40% / 0.8);
+  background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
 `;
@@ -60,14 +60,14 @@ const Nav = styled.nav`
   gap: 16px;
 `;
 const NavLink = styled.a`
-  color: ${COLORS.gray[900]};
+  color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
   text-decoration: none;
   font-size: 1.125rem;
   text-transform: uppercase;
 
   &:first-of-type:not(footer a) {
-    color: ${COLORS.secondary};
+    color: var(--color-secondary);
   }
 `;
 const CloseButton = styled(UnstyledButton)`
